19b7bab2e851c71a22d22aa36d0530e8d5f5dea0
wèijié <weijie@rongyi.com>
2023-07-30 17:11:33 +0800

    修复类型定义

--- src/components/el-pro-curd/model/object-model.ts
+++ src/components/el-pro-curd/model/object-model.ts
@@ -1,7 +1,7 @@
 import { BaseModel } from './base-model'
-import type { Model } from './index'
+import type { ModelItem } from './index'
 
 export interface ObjectModel extends BaseModel {
   数据类型: '对象'
-  数据模型: Model[]
+  数据模型: ModelItem[]
 }
