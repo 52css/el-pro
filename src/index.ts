d43a2aac6f64bc407064e371f14e142e7bb758f1
wèijié <weijie@rongyi.com>
2023-08-01 06:22:28 +0800

    调整全局导出

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import ElProButton from './components/el-pro-button'
+import ElProCurd from './components/el-pro-curd/index.vue'
 import ElProForm from './components/el-pro-form'
+import ElProOperation from './components/el-pro-operation'
 import ElProQuery from './components/el-pro-query'
+import ElProStatus from './components/el-pro-status'
 import ElProTable from './components/el-pro-table'
 
-const components = [ElProButton, ElProForm, ElProQuery, ElProTable]
+const components = [
+  ElProButton,
+  ElProCurd,
+  ElProForm,
+  ElProOperation,
+  ElProQuery,
+  ElProStatus,
+  ElProTable,
+]
 
 const install = function (Vue) {
   components.forEach((component) => {
